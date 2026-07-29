2bd7380c93b1bfe482d5ad7a705bdd9d9e19a1ab
nikollas-dh <nikollasdheyvis17@gmail.com>
2026-07-29 15:33:14 -0300

    Adicionar visualizar senha no cadastro

--- App_SaveFood/App_SaveFood/Pages/CadastroPage.xaml.cs
+++ App_SaveFood/App_SaveFood/Pages/CadastroPage.xaml.cs
@@ -6,4 +6,32 @@ public partial class CadastroPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void eyeButton_Clicked(object sender, EventArgs e)
+    {
+        if (txtSenha.IsPassword)
+        {
+            eyeButton.Source = "eye_off.png";
+            txtSenha.IsPassword = false;
+        }
+        else
+        {
+            eyeButton.Source = "eye.png";
+            txtSenha.IsPassword = true;
+        }
+    }
+
+    private void eyeButton2_Clicked(object sender, EventArgs e)
+    {
+        if (txtConfirmarSenha.IsPassword)
+        {
+            eyeButton2.Source = "eye_off.png";
+            txtConfirmarSenha.IsPassword = false;
+        }
+        else
+        {
+            eyeButton2.Source = "eye.png";
+            txtConfirmarSenha.IsPassword = true;
+        }
+    }
 }
