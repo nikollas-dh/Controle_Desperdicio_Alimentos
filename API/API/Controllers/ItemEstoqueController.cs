6a002254199ec44aafdca9010ad21a088333d4be
nikollas-dh <nikollasdheyvis17@gmail.com>
2026-08-05 15:50:56 -0300

    listar itens do estoque com problemas na base de dados

---
+++ API/API/Controllers/ItemEstoqueController.cs
@@ -0,0 +1,33 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemEstoqueController : ControllerBase
+    {
+      private readonly DbDesperdicioAlimentosContext ct = new DbDesperdicioAlimentosContext();
+
+        public ItemEstoqueController(DbDesperdicioAlimentosContext context) 
+        {
+            ct = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult itemRestaurante([FromRoute]int id) 
+        {
+            var estoque = ct.ItemEstoques.Where(u=>u.FkIdRestaurante == id)
+                .Select(i => new
+                {
+                    id= i.FkIdRestaurante,
+                    item = i.FkIdItemNavigation.Nome,
+                    quantidade = i.Quantidade+ i.FkIdUnidadeNavigation.Nome
+
+                }).ToList();
+                return Ok(estoque);
+        }
+    }
+}
